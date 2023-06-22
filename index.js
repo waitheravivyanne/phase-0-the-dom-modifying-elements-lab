1ef02d7ee23927fe4d87c222e7ad52d58193ff11
vivian waithera <vivyannekori@gmail.com>
2023-06-22 14:39:52 +0300

    Completed assignent

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const main = document.querySelector("main")
+
+main.remove()
+const newHeader = document.createElement("h1")
+
+newHeader.id = "victory"
+newHeader.textContent = "VIVIAN is the champion"
